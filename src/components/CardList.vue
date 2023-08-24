7c94b7bb194b1648d295989835dbebaf671988b9
PasqualeIsabelli <pasquale0395@gmail.com>
2023-08-24 20:53:32 +0200

    add store.js

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,6 @@
 <script>
-import axios from "axios";
 import SingleCard from "./SingleCard.vue"
+import { store, fetchCards } from "../store";
 
 export default {
   components: {
@@ -8,20 +8,11 @@ export default {
   },
   data() {
     return {
-      cards: [],
-    }
-  },
-  methods: {
-    fetchCards() {
-      const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0";
-
-      axios.get(url).then((response) => {
-        this.cards = response.data.data;
-      });
+      store
     }
   },
   mounted() {
-    this.fetchCards();
+    fetchCards();
   }
 }
 </script>
@@ -30,7 +21,7 @@ export default {
   <div class="p-5">
     <div class="bg-dark text-white p-3 fw-bold fs-4">FOUND</div>
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-3">
-        <div class="col" v-for="singleCard in cards" :key="singleCard.id">
+        <div class="col" v-for="singleCard in store.cards" :key="singleCard.id">
           <SingleCard :card="singleCard"></SingleCard>
         </div>
     </div>
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { reactive } from "vue";
+
+export const store = reactive({
+  card: [],
+});
+
+export function fetchCards() {
+  const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0";
+
+  axios.get(url).then((response) => {
+    store.cards = response.data.data;
+  });
+}
