e2c72a09fb5b09caed0ec08567eadcd67c2136d3
PasqualeIsabelli <pasquale0395@gmail.com>
2023-08-24 16:09:25 +0200

    add initial files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+
+  <main>
+    
+  </main>
+
+  <TheFooter></TheFooter>
+</template>
+
+<style lang="scss">
+
+</style>
