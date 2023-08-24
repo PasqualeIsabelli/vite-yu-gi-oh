60cbea1063e6a79a42f2f3fc214f050a4a2bb25e
PasqualeIsabelli <pasquale0395@gmail.com>
2023-08-24 21:49:03 +0200

    fix some details

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
 import CardList from './components/CardList.vue';
-import FilterDropdown from './components/FilterDropdown.vue';
+import Dropdown from './components/Dropdown.vue';
 
 export default {
   components: {
     TheHeader,
     TheFooter,
     CardList,
-    FilterDropdown
+    Dropdown
   },
   data() {
     return {
@@ -23,7 +23,7 @@ export default {
 
   <main>
     <div class="container">
-      <FilterDropdown></FilterDropdown>
+      <Dropdown></Dropdown>
       <div class="bg-white">
         <CardList></CardList>
       </div>
